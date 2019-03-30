869d056c01225347efac0bb9141acf773776244f
Corey Townsend <CoreyTownsend12@gmail.com>
2019-03-29 23:11:12 -0400

    Initial Commit

---
+++ EcoDrop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EcoDrop
+//
+//  Created by Corey Townsend on 3/29/19.
+//  Copyright © 2019 CodeWithEcoDrop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
